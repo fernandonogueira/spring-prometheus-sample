86c90c810439dc82a298d7486ff8269b406fc8cd
Fernando Nogueira <fernandojnn@gmail.com>
2019-10-24 17:08:59 -0300

    Adding sample histogram and percentiles. Updating version

--- build_and_push.sh
+++ build_and_push.sh
@@ -3,6 +3,6 @@
 mvn package
 
 docker build -t spring-prometheus-sample .
-docker tag spring-prometheus-sample fernandonogueira/spring-prometheus-sample:0.2
+docker tag spring-prometheus-sample fernandonogueira/spring-prometheus-sample:0.3
 
-docker push fernandonogueira/spring-prometheus-sample:0.2
+docker push fernandonogueira/spring-prometheus-sample:0.3
--- src/main/kotlin/fernandonogueira/springprometheus/jobs/SampleJob.kt
+++ src/main/kotlin/fernandonogueira/springprometheus/jobs/SampleJob.kt
@@ -1,22 +1,39 @@
 package fernandonogueira.springprometheus.jobs
 
 import io.micrometer.core.instrument.MeterRegistry
+import io.micrometer.core.instrument.Timer
+import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
+import java.time.Duration
 
 @Component
 class SampleJob(private val meterRegistry: MeterRegistry) {
 
-  val timer = meterRegistry.timer("sampleapp_sample_job_duration")
+  val timer = Timer.builder("sampleapp_sample_job_duration")
+    .publishPercentiles(0.5, 0.95, 0.99)
+    .minimumExpectedValue(Duration.ofMillis(1))
+    .maximumExpectedValue(Duration.ofSeconds(11))
+    .register(meterRegistry)
 
-  val logger = LoggerFactory.getLogger(SampleJob::class.java)
+  val histogram = Timer.builder("sampleapp_sample_job_histogram")
+    .publishPercentiles(0.5, 0.95, 0.99)
+    .publishPercentileHistogram()
+    .sla(Duration.ofMillis(100))
+    .minimumExpectedValue(Duration.ofMillis(1))
+    .maximumExpectedValue(Duration.ofSeconds(10))
+    .register(meterRegistry)
+
+  val logger: Logger = LoggerFactory.getLogger(SampleJob::class.java)
 
   @Scheduled(fixedDelay = 30000)
   fun sampleJob() {
     logger.info("Starting sample job...")
-    timer.record {
-      Thread.sleep(12000)
+    histogram.record {
+      timer.record {
+        Thread.sleep(12000)
+      }
     }
     logger.info("Sample job completed.")
   }
